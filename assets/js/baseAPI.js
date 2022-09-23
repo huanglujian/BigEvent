4a93fcc08130cd4d191c4eb043d85ed0bdce83d6
huanglujian <huanglujian6@163.com>
2022-09-23 10:57:27 +0800

    完成了index分支

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,16 @@ $.ajaxPrefilter(function (options) {
   // options.url = 'http://ajax.frontend.itheima.net' + options.url
   options.url = 'http://www.liulongbin.top:3007' + options.url
   /* 这个 options.url 就是 我们在 $.post 中 给的 url ，因为在那边 只给 URL后面的部分，这边再给平完整  */
+  // 添加 headers 属性
+  console.log(options)
+  if (options['url'].indexOf('/my/') !== -1) {
+    options.headers = { Authorization: localStorage.getItem('token') || '' }
+  }
+  // 添加 complete 完成回调函数 
+  options.complete = function (res) {
+    if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+      localStorage.removeItem('token')
+      location.href = './login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function () {
+  getUserInfo()
+})
+
+// 请求用户数据的函数
+function getUserInfo() {
+  $.ajax({
+    method: "GET",
+    url: '/my/userinfo',
+    success: function (res) {
+      console.log(res)
+      if (res.status !== 0) return layui.layer.msg(res.message)
+      renderAvatar(res.data)
+    }
+  })
+}
+// 渲染头像 的 函数
+function renderAvatar(data) {
+  const name = data['nickname'] || data['username']
+  $('.welcome').html(`欢迎  ${name}`)
+  if (data.user_pic !== null) {
+    $('.uesrinfo').attr('src', data.user_pic)
+    $('.text-avatar').hide()
+    $('.layui-nav-img').show()
+  } else {
+    $('.text-avatar').html(`${name[0].toUpperCase()}`)
+    $('.text-avatar').show()
+    $('.layui-nav-img').hide()
+  }
+}
+
+// 给 退出a标签 添加点击事件
+$('.logout').on('click', function () {
+  //eg1
+  layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
+    localStorage.removeItem('token')
+    location.href = './login.html'
+    // layer.close(index)
+  })
+
+})
